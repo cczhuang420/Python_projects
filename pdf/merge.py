68d63fed7b6b8c346d85e4edb57274fdd022a548
cczhuang420 <zhuangxiaoxiao1988@gmail.com>
2022-07-12 15:36:52 +1200

    modified merge.py

--- pdf/merge.py
+++ pdf/merge.py
@@ -1,11 +1,29 @@
 import PyPDF2
 import sys
+import os
 
-pdf_files = sys.argv[1:]
-merger = PyPDF2.PdfFileMerger()
 
-for pdf_file in pdf_files:
-    merger.append(pdf_file)
+def merge_from_folder():
+    input_folder = input(
+        "Please provide your directory to process pdf file:  ")
+    merger = PyPDF2.PdfFileMerger()
+    for file in os.listdir(input_folder):
+        file_path = os.path.join(input_folder, file)
+        merger.append(file_path)
+    merger.write('merged_files.pdf')
+    merger.close()
 
-merger.write('merged_files.pdf')
-merger.close()
+
+def merge_from_argv():
+    pdf_files = sys.argv[1:]
+    merger = PyPDF2.PdfFileMerger()
+
+    for pdf_file in pdf_files:
+        merger.append(pdf_file)
+    merger.write('merged_files.pdf')
+    merger.close()
+
+
+if __name__ == "__main__":
+    merge_from_folder()
+    # merge_from_argv()
